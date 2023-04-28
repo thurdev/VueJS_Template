eb49c484aacdd2fcc4bb72e674c98dd107c16f18
ThurDev <thurcoder@gmail.com>
2023-04-28 15:50:00 +0100

    refactor(store): change Vuex.createStore to Vuex.Store and add Vue.use(Vuex)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,14 @@
 import Vuex from 'vuex';
+import Vue from 'vue';
 
 import State from './state';
 import Getters from './getters';
 import Mutations from './mutations';
 import Actions from './actions';
 
-const store = new Vuex.createStore({
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
   state: State,
   mutations: Mutations,
   getters: Getters,
